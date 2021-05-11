a7947113b8521242dcf268668c6c0d7fdae863e5
Alexis <alexiscarreras@hotmail.com>
2021-05-11 11:45:12 -0300

    Api artista funcionando

---
+++ Spotify-main/Spotify.Api/Controllers/ArtistController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Spotify.Application;
+using Spotify.Domain.Abstract;
+using Spotify.Domain.Enums;
+using Spotify.Domain.Response;
+using System.Collections.Generic;
+
+namespace Spotify.Api.Controllers
+{
+    [Route("api/artist/{id}")]
+    [ApiController]
+    public class ArtistController : ControllerBase
+    {
+        private readonly ArtistBusiness _artistBusiness;
+        public ArtistController()
+        {
+            _artistBusiness = new ArtistBusiness();
+        }
+
+        [HttpGet]
+        public IActionResult Get(string id)
+        {
+            var response = _artistBusiness.artist(id);
+            return Ok(response);
+        }
+    }
+}
