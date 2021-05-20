fcd87bec3b80b7e8a1e619a276b36c58c3748506
Alexis <alexiscarreras@hotmail.com>
2021-05-20 17:01:12 -0300

    Autorizacion en prueba

---
+++ Spotify-main/Spotify.Application/AuthBusiness.cs
@@ -0,0 +1,26 @@
+﻿using Spotify.Domain.Response;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spotify.Application
+{
+	public class AuthBusiness
+	{
+		private Service.SpotifyService _authService { get; set; }
+        public AuthBusiness()
+        {
+            _authService = new Service.SpotifyService();
+        }
+
+        public Service.SpotifyService GetAuthService()
+        {
+            return _authService;
+        }
+
+        public Auth Auth()
+        {
+            
+        }
+    }
+}
