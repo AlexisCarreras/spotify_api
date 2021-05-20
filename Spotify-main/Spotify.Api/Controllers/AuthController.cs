4d7fd8b4dc1c7250c64f8a7b3d500fb824aea5a5
Alexis <alexiscarreras@hotmail.com>
2021-05-20 16:27:41 -0300

    Auth Controller de prueba funcionando

--- Spotify-main/Spotify.Api/Controllers/AuthController.cs
+++ Spotify-main/Spotify.Api/Controllers/AuthController.cs
@@ -17,9 +17,9 @@ namespace Spotify.Api.Controllers
 
     class SpotifyAuthentication
     {
-        public string clientID = "732c3f54134b4e6a875b156899fe2bfa";
-        public string clientSecret = "53224f31eace44fabc6767c85ce299db";
-        public string redirectURL = "http://mysite.com/callback/";
+        public string clientID = "5ea6a5fb8c494c0c87810637665e9a59";
+        public string clientSecret = "d904ac28c446456d956ceb30a5a40df5";
+        public string redirectURL = "https://localhost:44395/callback";
     }
 
     [Route("api/[Controller]/")]
@@ -55,7 +55,7 @@ namespace Spotify.Api.Controllers
             };
         }
 
-        [Route("callback")]
+        [Route("/callback")]
         public ContentResult Get(string code)
         {
             string responseString = "";
