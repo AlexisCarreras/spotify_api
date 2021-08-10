9a5645940969e8f8afca34329c2528e6824a1f7e
FerMossier <mossierfernando@gmail.com>
2021-08-10 08:29:07 -0300

    trackLenght de clase SearchDTO cambiado a String. trackLenght de clase SearchBusiness convertido a formato hh:mm:ss. Corregida la demora excesiva en la búsqueda de tracks

--- Spotify-main/src/Business/SearchBusiness.cs
+++ Spotify-main/src/Business/SearchBusiness.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Spotify.Core.Enums;
 using Spotify.Core.Interfaces;
 using Spotify.Core.Models.Search;
@@ -7,77 +8,82 @@ using Spotify.Core.Response;
 namespace Spotify.Business
 {
     public class SearchBusiness : ISearchBusiness
-	{
-		private ISpotifyService _searchService { get; set; }
-		public SearchBusiness(ISpotifyService searchService)
-		{
-			_searchService = searchService;
-		}
+    {
+        private ISpotifyService _searchService { get; set; }
+        public SearchBusiness(ISpotifyService searchService)
+        {
+            _searchService = searchService;
+        }
 
-		public List<SearchDTO> SearchArtist(string name, SearchEnum type)
-		{
-			var responseService = _searchService.Search(name, type.ToString());
-			var arrItem = ((ArtistSearch)responseService).artists.items;
-			List<SearchDTO> listArtista = new List<SearchDTO>();
+        public List<SearchDTO> SearchArtist(string name, SearchEnum type)
+        {
+            var responseService = _searchService.Search(name, type.ToString());
+            var arrItem = ((ArtistSearch)responseService).artists.items;
+            List<SearchDTO> listArtista = new List<SearchDTO>();
 
-			for (int i = 0; i < arrItem.Length; i++)
-			{
-				SearchDTO artist = new SearchDTO()
-				{
-					id = arrItem[i].id,
-					imagen_url = arrItem[i].images[0].url,
-					name_artist = arrItem[i].name,
-					type = arrItem[i].type,
-				};
-				listArtista.Add(artist);
-			}
-			return listArtista;
-		}
+            for (int i = 0; i < arrItem.Length; i++)
+            {
+                SearchDTO artist = new SearchDTO()
+                {
+                    id = arrItem[i].id,
+                    imagen_url = arrItem[i].images[0].url,
+                    name_artist = arrItem[i].name,
+                    type = arrItem[i].type,
+                };
+                listArtista.Add(artist);
+            }
+            return listArtista;
+        }
 
-		public List<SearchDTO> SearchAlbum(string name, SearchEnum type)
-		{
-			var responseService = _searchService.Search(name, type.ToString());
-			var arrItem = ((AlbumSearch)responseService).albums.items;
-			List<SearchDTO> listAlbum = new List<SearchDTO>();
+        public List<SearchDTO> SearchAlbum(string name, SearchEnum type)
+        {
+            var responseService = _searchService.Search(name, type.ToString());
+            var arrItem = ((AlbumSearch)responseService).albums.items;
+            List<SearchDTO> listAlbum = new List<SearchDTO>();
 
-			for (int i = 0; i < arrItem.Length; i++)
-			{
-				SearchDTO album = new SearchDTO()
-				{
-					id = arrItem[i].id,
-					imagen_url = arrItem[i].images[0].url,
-					name_artist = arrItem[i].artists[0].name,
-					name_album = arrItem[i].name,
-					type = arrItem[i].type,
-				};
-				listAlbum.Add(album);
-			}
-			return listAlbum;
-		}
+            for (int i = 0; i < arrItem.Length; i++)
+            {
+                SearchDTO album = new SearchDTO()
+                {
+                    id = arrItem[i].id,
+                    imagen_url = arrItem[i].images[0].url,
+                    name_artist = arrItem[i].artists[0].name,
+                    name_album = arrItem[i].name,
+                    type = arrItem[i].type,
+                };
+                listAlbum.Add(album);
+            }
+            return listAlbum;
+        }
 
-		public List<SearchDTO> SearchTrack(string name, SearchEnum type)
-		{
-			var responseService = _searchService.Search(name, type.ToString());
-			var arrItem = ((TrackSearch)responseService).tracks.items;
-			List<SearchDTO> listTrack = new List<SearchDTO>();
+        public List<SearchDTO> SearchTrack(string name, SearchEnum type)
+        {
+            var responseService = _searchService.Search(name, type.ToString());
+            var arrItem = ((TrackSearch)responseService).tracks.items;
+            List<SearchDTO> listTrack = new List<SearchDTO>();
 
-			for (int i = 0; i < arrItem.Length; i++)
-			{
-				var trackFeatures = _searchService.TrackFeatures(arrItem[i].id);
-				SearchDTO track = new SearchDTO()
-				{
-					id = arrItem[i].id,
-					imagen_url = arrItem[i].album.images[0].url,
-					name_artist = arrItem[i].artists[0].name,
-					name_track = arrItem[i].name,
-					track_lenght = arrItem[i].duration_ms,
-					favorite = false,
-					//previewUrl = arrItem[i].preview_url,
-					type = arrItem[i].type,
-				};
-				listTrack.Add(track);
-			}
-			return listTrack;
-		}
-	}
+            for (int i = 0; i < arrItem.Length; i++)
+            {
+                int ms = arrItem[i].duration_ms;
+                TimeSpan t = TimeSpan.FromMilliseconds(ms);
+                string answer = ms < 3600000 && ms > 0
+                    ? string.Format("{1:D2}:{2:D2}", t.Hours, t.Minutes, t.Seconds, t.Milliseconds)
+                    : string.Format("{0:D2}:{1:D2}:{2:D2}", t.Hours, t.Minutes, t.Seconds, t.Milliseconds);
+
+                SearchDTO track = new SearchDTO()
+                {
+                    id = arrItem[i].id,
+                    imagen_url = arrItem[i].album.images[0].url,
+                    name_artist = arrItem[i].artists[0].name,
+                    name_track = arrItem[i].name,
+                    track_lenght = answer,
+                    favorite = false,
+                    //previewUrl = arrItem[i].preview_url,
+                    type = arrItem[i].type,
+                };
+                listTrack.Add(track);
+            }
+            return listTrack;
+        }
+    }
 }
--- Spotify-main/src/Core/Response/SearchDTO.cs
+++ Spotify-main/src/Core/Response/SearchDTO.cs
@@ -7,7 +7,7 @@
 		public string name_track { get; set; }
 		public string name_album { get; set; }
 		public string name_artist { get; set; }
-		public int track_lenght { get; set; }
+		public string track_lenght { get; set; }
 		public bool favorite { get; set; }
 		public string previewUrl { get; set; }
 		public string type { get; set; }
